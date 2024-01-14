3bd46c7821822d714d26a7c08e76cbc569921ac4
Joaquin144 <vvpp898.comco@gmail.com>
2024-01-14 10:54:46 +0530

    create Retrofit API

---
+++ app/src/main/java/com/joaquin/cryptotoday/data/remote/api/CryptoApi.kt
@@ -0,0 +1,15 @@
+package com.joaquin.cryptotoday.data.remote.api
+
+import com.joaquin.cryptotoday.data.remote.responses.ListApiResponse
+import com.joaquin.cryptotoday.data.remote.responses.LiveApiResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface CryptoApi {
+    @GET("/live")
+    fun fetchLiveCoins(@Query("access_key") apiKey: String): Response<LiveApiResponse>
+
+    @GET("/list")
+    fun fetchListCoins(@Query("access_key") apiKey: String): Response<ListApiResponse>
+}
